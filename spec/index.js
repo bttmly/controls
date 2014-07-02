538a049f273f26df5fea5ef14718b9c41a7be00b
Nick Bottomley <nhbottomley@gmail.com>
2014-07-02 11:25:09 -0700

    Structuring & yo test

---
+++ spec/index.js
@@ -0,0 +1,20 @@
+/* global index, describe, it, expect, should */
+
+describe('index()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(index).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
